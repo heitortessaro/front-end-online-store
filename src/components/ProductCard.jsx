4b5766ad97acb7bd0bdeda2696a7c65dd4a707eb
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-03-10 18:32:27 -0300

    cria component ProductCard.

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ProductCard extends Component {
+  render() {
+    const {
+      productImg,
+      productName,
+      productPrice,
+    } = this.props;
+    return (
+      <div
+        className="product-card"
+        data-testid="product"
+      >
+        <h3>{ productName }</h3>
+        <img src={ productImg } alt={ productName } />
+        <span>{ productPrice }</span>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  productImg: PropTypes.string.isRequired,
+  productName: PropTypes.string.isRequired,
+  productPrice: PropTypes.string.isRequired,
+};
