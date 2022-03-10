5286d60b175d62e432e66509e201c94ce2c5e6ff
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-03-10 16:16:17 -0300

    completa funcao getCategories

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 export async function getCategories() {
-  // Implemente aqui
+  const ULR_CATEGORIES = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(ULR_CATEGORIES);
+  const data = await response.json();
+  return data;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
