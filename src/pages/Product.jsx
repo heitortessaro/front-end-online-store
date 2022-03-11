3cfbbc2658a23b067b70dbed621b10cc63efb140
DannyElis <dannyelis89@gmail.com>
2022-03-11 19:09:55 -0300

    Cria page product

---
+++ src/pages/Product.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+  state = { }
+
+  render() {
+    return (
+      <div>Pagina de produto detalhado </div>
+    );
+  }
+}
+
+export default Product;
