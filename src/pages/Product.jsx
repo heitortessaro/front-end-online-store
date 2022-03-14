b01d4421c475b92499362fbc44a6eeb6fce605ce
Cássius Queiroz Bessa <cassiusbessa@gmail.com>
2022-03-14 15:56:35 -0300

    Req 07 finalizado

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { getItem } from '../services/api';
 
 class Product extends Component {
-  state = { }
+  state = { product: {} }
+
+  componentDidMount() {
+    this.getDetails();
+  }
+
+  getDetails = async () => {
+    const { match: { params: {
+      id } } } = this.props;
+    const requestProduct = await getItem(id);
+    console.log(requestProduct);
+    this.setState({ product: requestProduct });
+  }
 
   render() {
-    const { match } = this.props;
-    // const { productName, match: { params: {
-    //   id,
-    // } } } = this.props;
-    console.log(match);
+    const { product } = this.state;
+    console.log(product);
     return (
       <div data-testid="product-detail-name">
-        Pagina de produto detalhado
-        <p>
-          teste
-          {' '}
-          {}
-          {' '}
-          teste
-        </p>
+        <h1>Detalhes do Produto</h1>
+        <h3>
+          {product.title}
+        </h3>
+        <img src={ product.thumbnail } alt={ product.title } />
+        <p>{product.price}</p>
       </div>
     );
   }
