1ad78fed51ce6e433675362bbd1b287fa4a643a0
DannyElis <dannyelis89@gmail.com>
2022-03-15 20:30:24 -0300

    Requisito 09 feito, refatora buttonAddToCard

--- src/components/ButtonAddToCart.jsx
+++ src/components/ButtonAddToCart.jsx
@@ -2,23 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ButtonAddToCart extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.addProductToCart = this.addProductToCart.bind(this);
-  // }
-  // add2Cart = (id) => {
-  //   const response = JSON.parse(window.localStorage.getItem(id));
-  //   if (response) {
-  //     window.localStorage.setItem(id, `${response + 1}`);
-  //   } else {
-  //     window.localStorage.setItem(id, '1');
-  //   }
-  // }
-
   render() {
     const {
       productObj,
       add2Cart,
+      datatesteid,
     } = this.props;
     return (
       <div
@@ -26,7 +14,7 @@ export default class ButtonAddToCart extends Component {
       >
         <button
           type="button"
-          data-testid="product-add-to-cart"
+          data-testid={ datatesteid }
           onClick={ () => add2Cart(productObj) }
         >
           Adicionar ao Carrinho
@@ -39,4 +27,5 @@ export default class ButtonAddToCart extends Component {
 ButtonAddToCart.propTypes = {
   productObj: PropTypes.shape({}).isRequired,
   add2Cart: PropTypes.func.isRequired,
+  datatesteid: PropTypes.string.isRequired,
 };
